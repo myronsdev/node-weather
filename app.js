808614b35e7541792df862ffef82f229a1324e0d
Myron Mctyer <myronsdev@gmail.com>
2019-03-20 23:03:20 -0500

    add mapbox api so that i can convert addresses to longitude/latitude

--- app.js
+++ app.js
@@ -6,8 +6,19 @@ const url =
 request({ url, json: true }, (err, res) => {
   // console.log(res.body.currently)
   const currentTemp = res.body.currently.temperature
-  const chanceOfrain = res.body.currently.precipProbability
-  const weatherString = `It is currently ${currentTemp} degrees out. There is a ${chanceOfrain}% chance of rain.`
+  const chanceOfRain = res.body.currently.precipProbability
+  const firstDaily = res.body.daily.data[0].summary
+  const weatherString = `${firstDaily} It is currently ${currentTemp} degrees out. There is a ${chanceOfRain}% chance of rain.`
 
   console.log(weatherString)
+})
+
+const mapBox =
+  'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibXlyb25zZGV2IiwiYSI6ImNqdGkyeHF5bjB2dWo0NHBqeWF5dWg2ZnQifQ.Kyd0lcsSVNWdRwbrfyEtUA&limit=1'
+
+request({ url: mapBox, json: true }, (error, response) => {
+  const longitude = response.body.features[0].center[0]
+  const latitude = response.body.features[0].center[1]
+
+  console.log(`${latitude} and ${longitude}`)
 })
