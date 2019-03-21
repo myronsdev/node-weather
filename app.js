7943d138223d90f31f79d7fcd201d33c41b5e88c
Myron Mctyer <myronsdev@gmail.com>
2019-03-20 21:36:28 -0500

    add log string with use of res object

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ const url =
   'https://api.darksky.net/forecast/5b18f1ade99122819bf60647095d48fd/37.8267,-122.4233'
 
 request({ url, json: true }, (err, res) => {
-  console.log(res.body.currently)
+  // console.log(res.body.currently)
+  const currentTemp = res.body.currently.temperature
+  const chanceOfrain = res.body.currently.precipProbability
+  const weatherString = `It is currently ${currentTemp} degrees out. There is a ${chanceOfrain}% chance of rain.`
+
+  console.log(weatherString)
 })
